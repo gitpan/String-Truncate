2cb277823d4e6882136314c087225c10c1960fa2
Ricardo SIGNES <rjbs@cpan.org>
2005-10-15 14:50:37 +0000

    Import of RJBS/String-Truncate-0.02 from CPAN.
    gitpan-cpan-distribution: String-Truncate
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         RJBS/String-Truncate-0.02.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/String/Truncate.pm
+++ lib/String/Truncate.pm
@@ -14,11 +14,11 @@ String::Truncate - a module for when strings are too long to be displayed in...
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
@@ -28,6 +28,7 @@ terminal width.  It can convert:
  "this is your brain" -> "this is your ..."
                       or "...is your brain"
                       or "this is... brain"
+                      or "... is your b..."
 
 It's simple:
 
@@ -38,6 +39,7 @@ It's simple:
  elide($brain, 16); # first option
  elide($brain, 16, { truncate => 'left' });   # second option
  elide($brain, 16, { truncate => 'middle' }); # third option
+ elide($brain, 16, { truncate => 'ends' });   # fourth option
 
 =head1 FUNCTIONS
 
@@ -49,7 +51,7 @@ elision.
 
 Valid arguments are:
 
- elide  - elide at left, right, or middle? (default: right)
+ elide  - elide at left, right, middle, or ends? (default: right)
  marker - how to mark the elision (default: ...)
 
 =cut
@@ -60,11 +62,14 @@ sub elide {
   $arg ||= {};
   my $truncate = $arg->{truncate} || 'right';
   croak "invalid value for truncate argument: $truncate"
-    unless grep { $_ eq $truncate } qw(left middle right);
+    unless grep { $_ eq $truncate } qw(left middle right ends);
 
   my $marker = defined $arg->{marker} ? $arg->{marker} : '...';
-  croak "elision marker $marker is longer than allowed length $length!"
+  croak "elision marker <$marker> is longer than allowed length $length!"
     if length($marker) > $length;
+
+  croak "two elision markers <$marker> are longer than allowed length $length!"
+    if (length($marker) * 2) > $length;
 
   # hey, this might be really easy:
   return $string if length($string) <= $length;
@@ -76,13 +81,21 @@ sub elide {
     return substr($string, 0, $keep) . $marker;
   } elsif ($truncate eq 'left') {
     return $marker . substr($string, -$keep, $keep);
-  } else { # middle
+  } elsif ($truncate eq 'middle') {
     my ($keep_left, $keep_right) = (int($keep / 2)) x 2;
     $keep_left +=1 if ($keep_left + $keep_right) < $keep;
     return substr($string, 0, $keep_left)
          . $marker
          . substr($string, -$keep_right, $keep_right);
+  } elsif ($truncate eq 'ends') {
+    $keep -= length($marker); # we're going to use two markers
+    my $midpoint = int(length($string) / 2);
+    my $keep_left = $midpoint - int($keep / 2);
+    return $marker
+         . substr($string, $keep_left, $keep)
+         . $marker;
   }
+  return;
 }
   
 =head2 C<< trunc($string, $length, \%arg) >>
@@ -102,6 +115,11 @@ sub trunc {
   return elide($string, $length, $arg);
 }
 
+=head1 SEE ALSO
+
+L<Text::Truncate> does a very similar thing, and lets you set the default
+marker, rather than specify one every time if you don't want "..."
+
 =head1 AUTHOR
 
 Ricardo Signes, C<< <rjbs at cpan.org> >>
@@ -115,6 +133,9 @@ I will be notified, and then you'll automatically be notified of progress on
 your bug as I make changes.
 
 =head1 ACKNOWLEDGEMENTS
+
+Ian Langworth gave me some good advice about naming things.  (Also some bad
+jokes.  Nobody wants String::ETOOLONG, Ian.)
 
 =head1 COPYRIGHT & LICENSE
 
